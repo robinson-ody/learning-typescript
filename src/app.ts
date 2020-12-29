010f98cb99e2f986360dd819168029c424e88d38
Robinson Ody <robinsonody@Robinson-Ody.local>
2020-12-30 02:07:35 +0700

    banyak

--- src/app.ts
+++ src/app.ts
@@ -1,103 +1,33 @@
-type Admin = {
-  name: string;
-  privilege: string[];
-};
+const nama: Array<string> = ['asd', 'qwe'];
 
-type Employee = {
-  name: string;
-  startDate: Date;
-};
+const promise: Promise<string> = new Promise(resolve => {
+  setTimeout(() => {
+    resolve('Resolved!');
+  }, 3000);
+});
 
-type elevatedEmployee = Admin & Employee;
-
-let e1: elevatedEmployee;
-
-e1 = {
-  name: 'Robin',
-  privilege: ['deploy-apps'],
-  startDate: new Date(),
-};
-
-type Combineable = string | number;
-type Numeric = number | boolean;
-
-type Universal = Combineable & Numeric;
-
-function addFn(a: number, b: number): number;
-function addFn(a: Combineable, b: Combineable) {
-  if (typeof a === 'string' || typeof b === 'string') return a.toString() + b.toString();
-  return a + b;
-  // return Number(a) + Number(b);
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
 }
 
-const hasil = addFn(1, 2);
+const mergedObj = merge({ name: 'Robin', hobbies: ['Making Music'] }, { age: 24 });
 
-type UnknownEmployee = Admin | Employee;
-
-const printEmployeeInfo = (emp: UnknownEmployee) => {
-  console.log(emp.name);
-  if ('privilege' in emp) console.log(emp.privilege);
-  if ('startDate' in emp) console.log(emp.startDate);
+type Lengthy = {
+  length: number;
 };
 
-printEmployeeInfo(e1);
-printEmployeeInfo({ name: 'Robinson Ody', startDate: new Date() });
-
-class Car {
-  drive() {
-    console.log(`Driving a car...`);
-  }
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let description = 'Got no value!';
+  const { length } = element;
+  if (length === 1) description = 'Got 1 value.';
+  if (length > 1) description = `Got ${length} value.`;
+  return [element, description];
 }
 
-class Truck {
-  drive() {
-    console.log(`Driving a truck...`);
-  }
+console.log(countAndDescribe('Hi!'));
 
-  loadCargo(amount: number) {
-    console.log(`Loading ${amount} kg of cargo...`);
-  }
+function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) {
+  return `Value: ${obj[key]}`;
 }
 
-type Vehicle = Car | Truck;
-
-const v1 = new Car();
-const v2 = new Truck();
-
-const useVehicle = (vehicle: Vehicle) => {
-  vehicle.drive();
-  if (vehicle instanceof Truck) vehicle.loadCargo(1000);
-};
-
-useVehicle(v1);
-useVehicle(v2);
-
-interface Bird {
-  type: 'bird';
-  flyingSpeed: number;
-}
-
-interface Horse {
-  type: 'horse';
-  runningSpeed: number;
-}
-
-type Animal = Bird | Horse;
-
-const moveAnimal = (animal: Animal) => {
-  let speed;
-
-  switch (animal.type) {
-    case 'bird':
-      speed = animal.flyingSpeed;
-      break;
-
-    case 'horse':
-      speed = animal.runningSpeed;
-  }
-
-  console.log(`Running at speed ${speed} km/h`);
-};
-
-moveAnimal({ type: 'bird', flyingSpeed: 100 });
-moveAnimal({ type: 'horse', runningSpeed: 50 });
+console.log(extractAndConvert({ age: 1, height: '200' }, 'height'));
---
+++ src/banyak deh.ts
@@ -0,0 +1,103 @@
+type Admin = {
+  name: string;
+  privilege: string[];
+};
+
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+type elevatedEmployee = Admin & Employee;
+
+let e1: elevatedEmployee;
+
+e1 = {
+  name: 'Robin',
+  privilege: ['deploy-apps'],
+  startDate: new Date(),
+};
+
+type Combineable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combineable & Numeric;
+
+function addFn(a: number, b: number): number;
+function addFn(a: Combineable, b: Combineable) {
+  if (typeof a === 'string' || typeof b === 'string') return a.toString() + b.toString();
+  return a + b;
+  // return Number(a) + Number(b);
+}
+
+const hasil = addFn(1, 2);
+
+type UnknownEmployee = Admin | Employee;
+
+const printEmployeeInfo = (emp: UnknownEmployee) => {
+  console.log(emp.name);
+  if ('privilege' in emp) console.log(emp.privilege);
+  if ('startDate' in emp) console.log(emp.startDate);
+};
+
+printEmployeeInfo(e1);
+printEmployeeInfo({ name: 'Robinson Ody', startDate: new Date() });
+
+class Car {
+  drive() {
+    console.log(`Driving a car...`);
+  }
+}
+
+class Truck {
+  drive() {
+    console.log(`Driving a truck...`);
+  }
+
+  loadCargo(amount: number) {
+    console.log(`Loading ${amount} kg of cargo...`);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+const useVehicle = (vehicle: Vehicle) => {
+  vehicle.drive();
+  if (vehicle instanceof Truck) vehicle.loadCargo(1000);
+};
+
+useVehicle(v1);
+useVehicle(v2);
+
+interface Bird {
+  type: 'bird';
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: 'horse';
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+const moveAnimal = (animal: Animal) => {
+  let speed;
+
+  switch (animal.type) {
+    case 'bird':
+      speed = animal.flyingSpeed;
+      break;
+
+    case 'horse':
+      speed = animal.runningSpeed;
+  }
+
+  console.log(`Running at speed ${speed} km/h`);
+};
+
+moveAnimal({ type: 'bird', flyingSpeed: 100 });
+moveAnimal({ type: 'horse', runningSpeed: 50 });
